6add40596b740f7dfdfdb081c688024411cd5f67
fier12 <fier12@fier12-PC.station>
2018-05-26 14:46:23 +0200

    Modificato metodo main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,6 @@
 package application;
 	
+import it.andrea.orariolavoro.dao.OrarioLavoroDAO;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -11,7 +12,14 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("OrarioLavoro.fxml"));
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("OrarioLavoro.fxml")) ;
+			//(BorderPane)FXMLLoader.load(getClass().getResource("OrarioLavoro.fxml"));
+			
+			BorderPane root = (BorderPane)loader.load();
+			OrarioLavoroController orarioLavoroController = loader.getController();
+			OrarioLavoroDAO orarioDAO = new OrarioLavoroDAO();
+			orarioLavoroController.setOrarioLavoroDAO(orarioDAO);
+			
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- src/application/OrarioLavoroController.java
+++ src/application/OrarioLavoroController.java
@@ -1,6 +1,73 @@
 package application;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+
+import it.andrea.orariolavoro.dao.OrarioLavoroDAO;
+import it.andrea.orariolavoro.model.OrarioLavoro;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+
 public class OrarioLavoroController {
-	
-	
+
+    @FXML
+    private TableView<OrarioLavoro> tblOrario;
+    
+    @FXML
+    private DatePicker txtData;
+
+    @FXML
+    private TextField txtOraArrico;
+
+    @FXML
+    private Button btnSalva;
+
+    @FXML
+    private TextField txtOraUscita;
+    
+    @FXML
+    private TableColumn<OrarioLavoro, LocalDate> colData;
+    
+    @FXML
+    private TableColumn<OrarioLavoro, LocalTime> colOraArrivo;
+    
+    private OrarioLavoroDAO orarioLavoroDAO;
+    
+    
+    
+ 
+    public void setOrarioLavoroDAO(OrarioLavoroDAO orarioLavoroDAO) {
+		this.orarioLavoroDAO = orarioLavoroDAO;
+	}
+
+
+	//METODI PRIVATI
+	private void caricaTabella(){
+    	
+    	
+    	
+    	ObservableList<OrarioLavoro> lst = FXCollections.observableArrayList(orarioLavoroDAO.getLista());
+    	System.out.println(lst.size());
+    	tblOrario.getColumns().clear();
+    	
+    	colData = new TableColumn<>("Data");
+    	colOraArrivo = new TableColumn<>("Ora Arrivo");
+    	
+    	colData.setCellValueFactory(new PropertyValueFactory<>("dataGiorno"));
+    	colOraArrivo.setCellValueFactory(new PropertyValueFactory<>("oraArrivo"));
+    	
+    	tblOrario.getColumns().addAll(colData,colOraArrivo);
+    	
+    	tblOrario.setItems(lst);
+    	
+    }
+
 }
